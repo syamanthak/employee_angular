04453384d9a14d8ad36514f6d9d74b5fd8d2a53e
Syamanthak <syamanthakr1000@gmail.com>
2023-04-01 18:26:54 +0530

    added to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +18,7 @@ import { FormComponent } from './form/form.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [[{provide:LocationStrategy,useClass:HashLocationStrategy}],],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
