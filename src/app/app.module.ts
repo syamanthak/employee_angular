216e994e4760c487c3b76bd896f8bcb9bdf822e5
Syamanthak <syamanthakr1000@gmail.com>
2023-04-01 18:29:20 +0530

    added to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { FormComponent } from './form/form.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [[{provide:LocationStrategy,useClass:HashLocationStrategy}],],
+  providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
